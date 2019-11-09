4ae55d2e7d568e8b35f8b687290f56838ec49a6d
Abdullah Kady <abdullahkady@gmail.com>
2019-11-09 22:10:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void GenGrid(String grid) {
+        // Write initial state predicates to KB.pl based on the grid.
+    }
+
+    public static void main(String args[]) {
+        GenGrid("5,5;1,2;3,4;1,1,2,1,2,2,3,3");
+    }
+}
